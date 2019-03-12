dff6a00022ae55aa94e11b8d308580bd86ba56f6
esarijal <esa.rijal@gmail.com>
2019-03-12 16:06:36 +0700

    @After and its friends

---
+++ src/main/java/com/mitrais/learn/aopdemo/aspect/AfterAopAspect.java
@@ -0,0 +1,47 @@
+package com.mitrais.learn.aopdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+// Configuration
+// AOP
+@Configuration
+@Aspect
+public class AfterAopAspect {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    // What kind of method calls I would intercept
+    //execution(* PACKAGE.*.*(..)) <- POINTCUT
+
+    // Combination of POINTCUT + ADVICE = ASPECT
+    // JoinPoint -> spesific execution instance (usually methods)
+    // Weaving(The process ) and Weaver (The framework)
+
+    @AfterReturning(
+            value = "execution(* com.mitrais.learn.aopdemo.business.*.*(..))",// <--- POINTCUT
+            returning = "result"
+    )
+    public void afterReturning(JoinPoint joinPoint, Object result){
+        // What to do? All below logic is ADVICE
+        logger.info("{} returned with value {}", joinPoint, result);
+    }
+
+    @AfterThrowing(
+            value = "execution(* com.mitrais.learn.aopdemo.business.*.*(..))",// <--- POINTCUT
+            throwing = "exception"
+    )
+    public void afterThrowing(JoinPoint joinPoint, Object exception){
+        // What to do? All below logic is ADVICE
+        logger.info("{} throw exception {}", joinPoint, exception);
+    }
+
+    @After(value = "execution(* com.mitrais.learn.aopdemo.business.*.*(..))")// <--- POINTCUT)
+    public void after(JoinPoint joinPoint){
+        // What to do? All below logic is ADVICE
+        logger.info("completed execution of {}", joinPoint);
+    }
+}
+;
